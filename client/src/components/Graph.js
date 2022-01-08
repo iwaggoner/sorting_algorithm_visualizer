662ff9976e5026e583aa23e8f4d2e3576ebbcf78
Mike Kohlberg <mkohlberg95@gmail.com>
2022-01-07 18:04:17 -0600

    quick sort still done

---
+++ client/src/components/Graph.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+const Graph = ( props) => {
+
+  const bars = props.array.map((element,index) => {
+    return <div key={index} className="bar" style={{height:`${element/3}px`}}>{element}</div>
+  })
+
+  useEffect(() => {
+    // console.log(props.array)
+  }, [props.array])
+
+  return (
+    <div className="flexContainer">
+      {bars}
+    </div>
+  )
+}
+
+export default Graph
