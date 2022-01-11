984a00be4a9f5d03a3b7de3ac096c1cfefc1dfda
Mike Kohlberg <mkohlberg95@gmail.com>
2022-01-11 11:27:56 -0600

    database working for Mike but not Isaac

--- server/config/db.js
+++ server/config/db.js
@@ -2,17 +2,19 @@
 
 // creating a base name for the mongodb
 // REPLACE THE STRING WITH YOUR OWN DATABASE NAME
-const mongooseBaseName = 'express-auth-template'
+const mongooseBaseName = 'db-algo-view'
 
 // create the mongodb uri for development and test
 const database = {
-	development: `mongodb://localhost/${mongooseBaseName}-development`,
+	// development: `mongodb://localhost/${mongooseBaseName}-development`,
+	development: `mongodb://127.0.0.1/${mongooseBaseName}`,
 	test: `mongodb://localhost/${mongooseBaseName}-test`,
 }
 
 // Identify if development environment is test or development
 // select DB based on whether a test file was executed before `server.js`
-const localDb = process.env.TESTENV ? database.test : database.development
+// const localDb = process.env.TESTENV ? database.test : database.development
+const localDb = database.development
 
 // Environment variable MONGODB_URI will be available in
 // heroku production evironment otherwise use test or development db
